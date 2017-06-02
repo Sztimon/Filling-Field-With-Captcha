b558c1cc64444068ff839162c88e0701682050c4
Sztimon <szymon.kropisz@gmail.com>
2017-06-02 18:50:55 +0200

    Nowy program
    Zainicjowanie programu testowego

---
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //podpięcie drivera
+        private IWebDriver GetChromeDriver()
+        {
+            var outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            return new ChromeDriver(outPutDirectory);
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            var driver = GetChromeDriver();
+            driver.Navigate().GoToUrl("http://www.qtptutorial.net/automation-practice");
+
+            //Find an element using an id
+            driver.FindElement(By.Id("idExample"));
+            var idelement = driver.FindElement(By.Id("idExample"));
+            idelement.Click();
+            driver.Navigate().Back();
+        }
+    }
+}
