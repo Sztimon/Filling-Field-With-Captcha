8a47e01f81626c4cdf7ddd573891761d93ba78e4
Sztimon <szymon.kropisz@gmail.com>
2017-06-11 11:51:30 +0200

    Test wypełnienia formularza
    Test, w którym wypełniany jest formularz i capcha

--- UnitTestProject1/UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using System.IO;
 using System.Reflection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -21,21 +22,31 @@ namespace UnitTestProject1
         public void Test1()
         {
             var driver = GetChromeDriver();
-            driver.Navigate().GoToUrl("http://www.ultimateqa.com");
-            Assert.AreEqual("Home - Ultimate QA", driver.Title);
-            driver.Navigate().GoToUrl("http://www.ultimateqa.com/automation");
-            Assert.AreEqual("Automation Practice - Ultimate QA", driver.Title);
-            var locator = By.XPath(".//*[@href='/complicated-page']");
-            var element = driver.FindElement(locator);
-            element.Click();
-            Assert.AreEqual("Complicated Page - Ultimate QA", driver.Title);
-            driver.Navigate().Back();
-            Assert.AreEqual("Automation Practice - Ultimate QA", driver.Title);
+            driver.Navigate().GoToUrl("http://www.ultimateqa.com/filling-out-forms/");   
+            
+            var locator = By.Id("et_pb_contact_name_1");
+            var nameField = driver.FindElements(locator);
+            nameField[1].Clear();
+            nameField[1].SendKeys("Przykładowa nazwa");
 
-            //find an element using link text
-            //var linktextelement = driver.findelement(by.linktext("Click Me"));
-            //linktextelement.click();
-            //driver.navigate().back();
+            var locator1 = By.Id("et_pb_contact_message_1");
+            var messageField = driver.FindElements(locator1);
+            messageField[1].Clear();
+            messageField[1].SendKeys("Przykładowy tekst");
+
+           
+            var locator2 = By.ClassName("et_pb_contact_captcha_question");
+            var captcha = driver.FindElement(locator2);
+            var table = new DataTable();
+            var captchaAnswer = (int)table.Compute(captcha.Text,"");
+
+            //wpisanie captcha do okienka
+            var captchaTextBox = driver.FindElement(By.XPath("//*[@class='input et_pb_contact_captcha']"));
+            captchaTextBox.SendKeys(captchaAnswer.ToString());
+
+            driver.FindElements(By.XPath("//*[@class='et_pb_contact_submit et_pb_button']"))[1].Submit();
+
+
         }
     }
 }
